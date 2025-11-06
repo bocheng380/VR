ffb86687ccdbfa8e5f17168510ef5d2c6680ea13
bocheng380 <linchris1210@gmail.com>
2025-11-06 11:57:57 +0800

    te

---
+++ Temple/data.js
@@ -0,0 +1,276 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "北海道神宮第二鳥居",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.1031834478346028,
+        "pitch": -0.07021558235459935,
+        "fov": 1.2631636046189838
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9801071913146657,
+          "pitch": 0.015175619061455592,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "北海道神宮入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -3.0049454679429193,
+        "pitch": -0.009373486693526445,
+        "fov": 1.1983138419433097
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1108381452908773,
+          "pitch": 0.15356055559960602,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 2.3637947614714783,
+          "pitch": 0.21952328365489038,
+          "rotation": 4.71238898038469,
+          "target": "2-"
+        },
+        {
+          "yaw": -3.0891800249160877,
+          "pitch": 0.47517373205980995,
+          "rotation": 3.141592653589793,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.122578820662264,
+          "pitch": -0.08362629722550352,
+          "title": "北海道神宮",
+          "text": "北海道神宮是北海道最重要神社，地位為⌈北海道總鎮守⌋。它起源於1869年，最初是為紀念明治天皇的詔令，供奉開拓北海道的⌈開拓三神⌋(大國魂神、大那牟遲神、少彥名神)，後於1964年曾祀明治天皇，成為現今祭祀四神的神社。"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "淨手處",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 2.27848038620745,
+        "pitch": 0.04533410523089998,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.796961278482419,
+          "pitch": 0.3734826438772938,
+          "rotation": 0.7853981633974483,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "神宮神門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -7.105427357601002e-15,
+        "pitch": -0.15265613991342342,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.007378726274568592,
+          "pitch": 0.3845778815721719,
+          "rotation": 3.141592653589793,
+          "target": "1-"
+        },
+        {
+          "yaw": 4.596502023446192e-8,
+          "pitch": 0.04219921674343752,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "參拜處",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -0.6536079953897165,
+        "pitch": -0.17046548752632873,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.545196854130027,
+          "pitch": 0.22193037374476532,
+          "rotation": 13.351768777756625,
+          "target": "5-"
+        },
+        {
+          "yaw": 2.5067073449494712,
+          "pitch": 0.27182441591819284,
+          "rotation": 6.283185307179586,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "御手販售處",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 1.695285578304552,
+        "pitch": 0.1344310131621711,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5062295260587533,
+          "pitch": 0.3768631365738866,
+          "rotation": 6.283185307179586,
+          "target": "3-"
+        },
+        {
+          "yaw": -2.006640144009392,
+          "pitch": 0.2478569818174634,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "北海道神宮",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
