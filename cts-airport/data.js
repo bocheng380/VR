746b94350d699f477ea7ece99acef42046fc3792
bocheng380 <linchris1210@gmail.com>
2025-11-13 11:59:01 +0800

    a

---
+++ cts-airport/data.js
@@ -0,0 +1,306 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "新千歲空港",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.0067037685799827,
+        "pitch": -0.07040790439413769,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7082171841815645,
+          "pitch": 0.10686557437181676,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.3966163402915086,
+          "pitch": 0.08601005931559413,
+          "rotation": 5.497787143782138,
+          "target": "2-"
+        },
+        {
+          "yaw": 2.4087269778584597,
+          "pitch": 0.053012764860588035,
+          "rotation": 0.7853981633974483,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4105241703824163,
+          "pitch": -0.1851681854790197,
+          "title": "新千歲空港",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-family:&quot;新細明體&quot;,serif;mso-ascii-font-family:\nArial;mso-fareast-theme-font:minor-fareast;mso-hansi-font-family:Arial;\nmso-bidi-font-family:Arial;color:#F2F2F2;mso-themecolor:background1;mso-themeshade:\n242;mso-style-textfill-fill-color:#F2F2F2;mso-style-textfill-fill-themecolor:\nbackground1;mso-style-textfill-fill-alpha:100.0%;mso-style-textfill-fill-colortransforms:\nlumm=95000\">新千歲機場（日語：<span title=\"漢字或假名表記（原文）\"><span title=\"日語文本\">新千歳空港</span></span>／<span title=\"平假名表記\"><span title=\"日語文本\">しんちとせくうこう</span></span></span><span lang=\"EN-US\" style=\"font-family:&quot;Arial&quot;,sans-serif;color:#F2F2F2;mso-themecolor:\nbackground1;mso-themeshade:242;mso-style-textfill-fill-color:#F2F2F2;\nmso-style-textfill-fill-themecolor:background1;mso-style-textfill-fill-alpha:\n100.0%;mso-style-textfill-fill-colortransforms:lumm=95000\">&nbsp;</span><span lang=\"EN-US\" style=\"color:#F2F2F2;mso-themecolor:background1;mso-themeshade:242;\nmso-style-textfill-fill-color:#F2F2F2;mso-style-textfill-fill-themecolor:background1;\nmso-style-textfill-fill-alpha:100.0%;mso-style-textfill-fill-colortransforms:\nlumm=95000\"><span title=\"羅馬字表記\">shin chitose kūkō</span><span lang=\"EN-US\" style=\"font-family: Arial, sans-serif;\">&nbsp;<a href=\"https://ja.wikipedia.org/wiki/%E6%96%B0%E5%8D%83%E6%AD%B3%E7%A9%BA%E6%B8%AF\" title=\"ja:新千歳空港\" style=\"border-radius: 2px;overflow-wrap: break-word\"><span style=\"font-family: Calibri, sans-serif; color: rgb(242, 242, 242);\">*</span></a>/<a href=\"https://zh.wikipedia.org/wiki/Help:%E6%97%A5%E8%AA%9E\" title=\"Help:日語\" style=\"border-radius: 2px;overflow-wrap: break-word\"><span style=\"font-family: Calibri, sans-serif; color: rgb(242, 242, 242);\">?</span></a></span><span style=\"font-family: 新細明體, serif;\">，</span><span lang=\"EN-US\" style=\"font-family: Arial, sans-serif;\"><a href=\"https://zh.wikipedia.org/wiki/%E5%9B%BD%E9%99%85%E8%88%AA%E7%A9%BA%E8%BF%90%E8%BE%93%E5%8D%8F%E4%BC%9A%E6%9C%BA%E5%9C%BA%E4%BB%A3%E7%A0%81\" title=\"國際航空運輸協會機場代碼\" style=\"border-radius: 2px; overflow-wrap: break-word;\"><span style=\"font-family: Calibri, sans-serif; color: rgb(242, 242, 242);\">IATA</span><span lang=\"EN-US\" style=\"font-family: 新細明體, serif; color: rgb(242, 242, 242);\"><span lang=\"EN-US\">代碼</span></span></a></span><span style=\"font-family: 新細明體, serif;\">：</span><span lang=\"EN-US\" style=\"font-family: Arial, sans-serif;\"><span title=\"英語文本\">CTS</span></span><span style=\"font-family: 新細明體, serif;\">；</span><span lang=\"EN-US\" style=\"font-family: Arial, sans-serif;\"><a href=\"https://zh.wikipedia.org/wiki/%E5%9C%8B%E9%9A%9B%E6%B0%91%E8%88%AA%E7%B5%84%E7%B9%94%E6%A9%9F%E5%A0%B4%E4%BB%A3%E7%A2%BC\" title=\"國際民航組織機場代碼\" style=\"border-radius: 2px; overflow-wrap: break-word;\"><span style=\"font-family: Calibri, sans-serif; color: rgb(242, 242, 242);\">ICAO</span><span lang=\"EN-US\" style=\"font-family: 新細明體, serif; color: rgb(242, 242, 242);\"><span lang=\"EN-US\">代碼</span></span></a></span><span style=\"font-family: 新細明體, serif;\">：</span><span lang=\"EN-US\" style=\"font-family: Arial, sans-serif;\"><span title=\"英語文本\">RJCC</span></span><span style=\"font-family: 新細明體, serif;\">）位於</span><span lang=\"EN-US\" style=\"font-family: Arial, sans-serif;\"><a href=\"https://zh.wikipedia.org/wiki/%E6%97%A5%E6%9C%AC\" title=\"日本\" style=\"border-radius: 2px; overflow-wrap: break-word;\"><span lang=\"EN-US\" style=\"font-family: 新細明體, serif; color: rgb(242, 242, 242);\"><span lang=\"EN-US\">日本</span></span></a><a href=\"https://zh.wikipedia.org/wiki/%E5%8C%97%E6%B5%B7%E9%81%93\" title=\"北海道\" style=\"border-radius: 2px; overflow-wrap: break-word;\"><span lang=\"EN-US\" style=\"font-family: 新細明體, serif; color: rgb(242, 242, 242);\"><span lang=\"EN-US\">北海道</span></span></a><a href=\"https://zh.wikipedia.org/wiki/%E5%8D%83%E6%AD%B2%E5%B8%82\" title=\"千歲市\" style=\"border-radius: 2px; overflow-wrap: break-word;\"><span lang=\"EN-US\" style=\"font-family: 新細明體, serif; color: rgb(242, 242, 242);\"><span lang=\"EN-US\">千歲市</span></span></a></span><span style=\"font-family: 新細明體, serif;\">與</span><span lang=\"EN-US\" style=\"font-family: Arial, sans-serif;\"><a href=\"https://zh.wikipedia.org/wiki/%E8%8B%AB%E5%B0%8F%E7%89%A7%E5%B8%82\" title=\"苫小牧市\" style=\"border-radius: 2px; overflow-wrap: break-word;\"><span lang=\"EN-US\" style=\"font-family: 新細明體, serif; color: rgb(242, 242, 242);\"><span lang=\"EN-US\">苫小牧市</span></span></a></span><span style=\"font-family: 新細明體, serif;\">交界，是</span><span lang=\"EN-US\" style=\"font-family: Arial, sans-serif;\"><a href=\"https://zh.wikipedia.org/wiki/%E6%9C%AD%E5%B9%8C%E5%B8%82\" title=\"札幌市\" style=\"border-radius: 2px; overflow-wrap: break-word;\"><span lang=\"EN-US\" style=\"font-family: 新細明體, serif; color: rgb(242, 242, 242);\"><span lang=\"EN-US\">札幌市</span></span></a></span><span style=\"font-family: 新細明體, serif;\">的主機場，於<span lang=\"EN-US\" style=\"font-family: Arial, sans-serif;\">1988</span>年啟用，替代了其於</span><span lang=\"EN-US\" style=\"font-family: Arial, sans-serif;\"><a href=\"https://zh.wikipedia.org/wiki/%E8%88%AA%E7%A9%BA%E8%87%AA%E8%A1%9B%E9%9A%8A\" title=\"航空自衛隊\" style=\"border-radius: 2px; overflow-wrap: break-word;\"><span lang=\"EN-US\" style=\"font-family: 新細明體, serif; color: rgb(242, 242, 242);\"><span lang=\"EN-US\">航空自衛隊</span></span></a></span><span lang=\"EN-US\"><a href=\"https://zh.wikipedia.org/w/index.php?title=%E5%8D%83%E6%AD%B2%E5%9F%BA%E5%9C%B0&amp;action=edit&amp;redlink=1\" title=\"千歲基地（頁面不存在）\" style=\"border-radius: 2px;overflow-wrap: break-word\"><span data-orig-title=\"千歲基地\" data-lang-code=\"ja\" data-lang-name=\"日語\" data-foreign-title=\"千歳基地\"><span lang=\"EN-US\" style=\"font-family: 新細明體, serif; color: rgb(242, 242, 242);\"><span lang=\"EN-US\">千歲基地</span></span></span></a></span><span style=\"font-family: 新細明體, serif;\">附近建設的舊千歲機場。</span></span><span lang=\"EN-US\" style=\"font-family:&quot;Arial&quot;,sans-serif;color:#F2F2F2;mso-themecolor:\nbackground1;mso-themeshade:242;mso-style-textfill-fill-color:#F2F2F2;\nmso-style-textfill-fill-themecolor:background1;mso-style-textfill-fill-alpha:\n100.0%;mso-style-textfill-fill-colortransforms:lumm=95000\"><o:p></o:p></span></p>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "新千歲空港入口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5603330782516345,
+        "pitch": 0.07552420424081419,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5986278933039308,
+          "pitch": 0.12298967095401814,
+          "rotation": 6.283185307179586,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.7099260819805195,
+          "pitch": 0.16559374087013268,
+          "rotation": 8.63937979737193,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.5927371024121122,
+          "pitch": 0.44258909277681013,
+          "rotation": 4.71238898038469,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "國際線出境大廳",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.010599570257804558,
+          "pitch": 0.508507987050205,
+          "rotation": 9.42477796076938,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.5480343963419916,
+          "pitch": 0.1542311034873194,
+          "rotation": 0.7853981633974483,
+          "target": "4-"
+        },
+        {
+          "yaw": -0.5564697231939437,
+          "pitch": 0.24653777637523966,
+          "rotation": 3.9269908169872414,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "國內線出發大廳",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 2.474398376730629,
+        "pitch": 0.0838827722201394,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.320859466129991,
+          "pitch": 0.39657734941510014,
+          "rotation": 3.141592653589793,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.9222603878109092,
+          "pitch": 0.3575613279909504,
+          "rotation": 3.141592653589793,
+          "target": "1-"
+        },
+        {
+          "yaw": 2.8027810831573525,
+          "pitch": 0.29888256731977414,
+          "rotation": 8.63937979737193,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "國際線免稅店",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4312911833885185,
+          "pitch": 0.16645192885702897,
+          "rotation": 0.7853981633974483,
+          "target": "2-"
+        },
+        {
+          "yaw": -1.9307151790871249,
+          "pitch": 0.17668248679006027,
+          "rotation": 4.71238898038469,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "國內線商店",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -3.0635727623449256,
+        "pitch": 0.04837494521494534,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1042805903406876,
+          "pitch": 0.1644731291466872,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 3.108856793166643,
+          "pitch": 0.44794523014237164,
+          "rotation": 3.141592653589793,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "CTS Airport",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ home/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-home",
+      "name": "home",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.5922439167465496,
+        "pitch": 0.0295714106200613,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "home",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
